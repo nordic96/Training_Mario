afa37dcd954e174c0975eedc459d915c3b68580c
nordic96 <rhrlgns96@gmail.com>
2019-06-01 19:42:10 +0800

    added DQN Agent

--- TrainMario.py
+++ TrainMario.py
@@ -1,15 +1,42 @@
 import gym_super_mario_bros
+import cv2
+from DQNAgent import DQNAgent
 from nes_py.wrappers import BinarySpaceToDiscreteSpaceEnv
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 
-env = gym_super_mario_bros.make('SuperMarioBros-v0')
-env = BinarySpaceToDiscreteSpaceEnv(env, SIMPLE_MOVEMENT)
+EPISODES = 5000
 
-done = True
-for step in range(5000):
-    if done:
+def train_dqn(env_id):
+    env = gym_super_mario_bros.make(env_id)
+    env = BinarySpaceToDiscreteSpaceEnv(env, SIMPLE_MOVEMENT)
+
+    state_size = env.observation_space.shape[0]
+    action_size = env.action_space.n
+    agent = DQNAgent(state_size, action_size)
+    done = False
+    batch_size = 32
+    
+    for e in range(EPISODES):
         state = env.reset()
-    state, reward, done, info = env.step(env.action_space.sample())
-    env.render()
+        state = cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)
+        env.render()
+        for time in range(500):
+            action = agent.act(state)
+            next_state, reward, done, _  = env.step(action)
+            next_state = cv2.cvtColor(next_state, cv2.COLOR_BGR2GRAY)
+            reward = reward if not done else -10
 
-env.close()
+            agent.remember(state, action, reward, next_state, done)
+            state = next_state
+            if done:
+                agent.update_target_model()
+                print("episode: {}/{}, score: {}. e: {:.2}".format(e, EPISODES, time, agent.epsilon))
+                break
+            if len(agent.memory) > batch_size:
+                agent.replay(batch_size)
+
+            if e % 10 == 0:
+                agent.save("./save/mario-ddqn.h5")
+        
+if __name__ == '__main__':
+    train_dqn('SuperMarioBros-v0')
